97122c0dcccc02221e3dfa7b55d76569dcb19f50
Javier Alarcón Sánchez <alarconsanchezjavier@gmail.com>
2024-10-30 20:14:37 +0100

    cameras.json

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
 const express = require("express");
 const axios = require("axios");
-const NodeCache = require("node-cache");
-const myCache = new NodeCache({stdTTL: 3600, checkperiod: 120});
+const fs = require("fs");
 
 const app = express();
 const port = 30049;
 
-const BASE_URL =
-  "https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/cameres-trafic-camaras-trafico/records";
-
 async function getCameras() {
-  if (myCache.has("cameras")) {
-    return myCache.get("cameras");
-  }
-  let pagesRemaining = true;
-  let page = 1;
-  let cameras = [];
-  while (pagesRemaining) {
-    const response = await axios.get(`${BASE_URL}?limit=${100}&offset=${page * 100}`);
-    cameras.push(
-      ...response.data.results.map((camera) => ({
-        id: camera.idcamara,
-        name: camera.descripcio,
-        type: camera.tipocamara,
-        isTunnel: camera.pasoinferi?.toLowerCase() === "si" ? true : false,
-        vlcUrl: `rtsp://camaras.valencia.es/stream/${camera.idcamara}/1`,
-        webUrl: camera.url,
-        coordinates: {
-          lat: camera.geo_point_2d.lat,
-          lon: camera.geo_point_2d.lon,
-        },
-      })),
-    );
-    pagesRemaining = response.data.results.length === 100;
-    page++;
-  }
-  myCache.set("cameras", cameras);
-  return cameras;
+  //read the file cameras.json
+  const cameras = JSON.parse(fs.readFileSync("cameras.json", "utf8"));
+  return cameras.map((camera) => ({
+    id: camera.idcamara,
+    name: camera.descripcio,
+    type: camera.tipocamara,
+    isTunnel: camera.pasoinferi?.toLowerCase() === "si" ? true : false,
+    vlcUrl: `rtsp://camaras.valencia.es/stream/${camera.idcamara}/1`,
+    webUrl: camera.url,
+    coordinates: {
+      lat: camera.geo_point_2d.lat,
+      lon: camera.geo_point_2d.lon,
+    },
+  }));
 }
 
 app.get("/", (req, res) => {
